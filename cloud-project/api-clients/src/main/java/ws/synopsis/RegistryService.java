839ec97235e813ccee0446657fc6e79fee8de09c
Elvis Perez <elvisperez.tec@gmail.com>
2022-07-07 15:50:53 -0500

    Configuracion para ssl

--- cloud-project/api-clients/src/main/java/ws/synopsis/RegistryService.java
+++ cloud-project/api-clients/src/main/java/ws/synopsis/RegistryService.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"file://${cloudproject.home}/api-clients.properties"})
+@PropertySource(value = {"file://${cloudproject.home}/api-clients.properties", "file://${cloudproject.ssl.home}/project-ssl.properties"})
 public class RegistryService {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryService.class, args);
